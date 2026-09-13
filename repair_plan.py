7c3d2c9b2a51943e84a784692364281c0dded0ae
luisfabricioviana-dev <luisfabricioviana@gmail.com>
2026-09-13 11:58:50 -0300

    v0.2.9: expand minimum-scope repair mapping

--- repair_plan.py
+++ repair_plan.py
@@ -4,5 +4,9 @@ def repair_scope(failure_class):
         'POLICY': 'REPAIR_POLICY_ROUTE',
         'EXECUTION': 'REPAIR_EXECUTION_SPEC',
         'VALIDATION': 'REPAIR_OUTPUT',
+        'TOOL': 'REPAIR_TOOL_STEP',
+        'PROVIDER': 'REPAIR_PROVIDER_STEP',
+        'DATA': 'REPAIR_DATA_INPUT',
+        'RENDER': 'REPAIR_RENDER_ONLY',
     }
     return scopes[failure_class]
